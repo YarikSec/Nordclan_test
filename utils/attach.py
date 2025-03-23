6bd7f418929ea49a8106e976601de163d182c133
Yaroslav Slipchishin <yarik.wade@gmail.com>
2025-03-23 22:02:35 +0300

    Редактировал conftest и attach

--- utils/attach.py
+++ utils/attach.py
@@ -1,24 +1,24 @@
 import allure
 from allure_commons.types import AttachmentType
 
-
+# Скриншот
 def add_screenshot(browser):
     png = browser.driver.get_screenshot_as_png()
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
-
+# Логи браузера
 def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
-
+# Страница в виде html
 def add_html(browser):
     html = browser.driver.page_source
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
 
-
-def add_video(browser):
-    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+# Видео сессии
+def add_video(browser, selenoid_url):
+    video_url = f'https://{selenoid_url}/video/' + browser.driver.session_id + '.mp4'
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
